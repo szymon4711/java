a03b6a0c937c91754248a907a24381f455f6a82e
szymon4711 <szymon4710@gmail.com>
2022-10-02 23:57:59 +0200

    Revert "Upload project"
    This reverts commit a086bf5b36cb20049bfdb0d403bce7d4ba545e95.

--- currenciesCalculatorGUI/src/pl/retsuz/data/IRemoteDataProvider.java
+++
@@ -1,8 +0,0 @@
-package pl.retsuz.data;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-
-public interface IRemoteDataProvider {
-    public String acquireRemoteData(String address) throws IOException;
-}
--- currenciesCalculatorGUI/src/pl/retsuz/exchange/IExchange.java
+++
@@ -1,7 +0,0 @@
-package pl.retsuz.exchange;
-
-import pl.retsuz.currency.ICurrency;
-
-public interface IExchange {
-    double exchange(ICurrency src, ICurrency tgt, double amt); //Oblicza otrzymaną ilość środka pieniężnego po podaniu wartości źródłowej, waluty źródłowej oraz waluty docelowej
-}
